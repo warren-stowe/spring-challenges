a1c84e95e65de84f9454fb963c077ff62a296091
Warren A Stowe <stowewa@gmail.com>
2021-07-14 22:11:16 -0400

    initial commit

---
+++ src/main/java/com/spring/challenges/ChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.spring.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/challenges/ChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
